7c0b3680dd97fb42e0283f927ceed87ca49730cd
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-01-17 21:50:58 +0800

    fix

--- dags/tests/wxchat_bot_by_appium.py
+++ dags/tests/wxchat_bot_by_appium.py
@@ -19,7 +19,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models import Variable
-from common.wxchat_sdk import WXAppOperator
+from dags.common.wxchat_bot_by_appium import WXAppOperator
 
 
 def send_message_to_wx(message: str, receiver: str, aters: str = "") -> bool:
