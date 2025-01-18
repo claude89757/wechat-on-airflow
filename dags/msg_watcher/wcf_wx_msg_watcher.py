d03fe4228639a09a7b89c941d201d64396e98f88
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-01-18 14:28:06 +0800

    fix

--- dags/msg_watcher/wcf_wx_msg_watcher.py
+++ dags/msg_watcher/wcf_wx_msg_watcher.py
@@ -69,6 +69,11 @@ def process_wx_message(**context):
     # 分类处理
     if msg_type == 1 and (content.startswith('@Zacks') or not is_group):
 
+        # 清理历史消息
+        if content.lower().strip('@zacks').strip() == 'clear':
+            Variable.set(f'{room_id}_msg_data', [], serialize_json=True)
+            return
+
         # 缓存聊天的历史消息
         room_msg_data = Variable.get(f'{room_id}_msg_data', default_var=[], deserialize_json=True)
         simple_message_data = {
