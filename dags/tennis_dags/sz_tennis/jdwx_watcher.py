a9548eb2ae6b6b84a2657c88f25b712e480be3eb
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-02-18 12:01:45 +0800

    fix

--- dags/tennis_dags/sz_tennis/jdwx_watcher.py
+++ dags/tennis_dags/sz_tennis/jdwx_watcher.py
@@ -204,14 +204,15 @@ def check_tennis_courts():
 
         # 获取微信发送配置
         wcf_ip = Variable.get("WCF_IP", default_var="")
-        for msg in up_for_send_msg_list:
-            send_wx_msg(
-                wcf_ip=wcf_ip,
-                message=msg,
-                receiver="38763452635@chatroom",
-                aters=''
-            )
-            sended_msg_list.append(msg)
+        for chat_room_id in ["57497883531@chatroom", "38763452635@chatroom"]:
+            for msg in up_for_send_msg_list:
+                send_wx_msg(
+                    wcf_ip=wcf_ip,
+                    message=msg,
+                    receiver=chat_room_id,
+                    aters=''
+                )
+                sended_msg_list.append(msg)
 
         # 更新Variable
         description = f"深圳金地网球场场地通知 - 最后更新: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
