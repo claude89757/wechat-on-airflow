3b17bc51d878ff7ce82987ba8b5c5fd28102ac2e
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-02-23 22:42:32 +0800

    fix

--- dags/msg_watcher/wcf_wx_msg_watcher.py
+++ dags/msg_watcher/wcf_wx_msg_watcher.py
@@ -294,35 +294,10 @@ def handler_text_msg(**context):
         check_for_unfinished_msg(dify_agent=dify_agent, conversation_id=conversation_id)
     else:
         pass
-
-    # Airflow Variable缓存的消息列表
-    # room_sender_msg_list = Variable.get(f'{WX_USER_ID}_{room_id}_{sender}_msg_list', default_var=[], deserialize_json=True)
-
-    # if enable_ai:
-    #     # 如果开启AI，则遍历近期的消息是否已回复，没有回复，则合并到这次提问
-    #     up_for_reply_msg_list = []
-    #     up_for_reply_msg_id_list = []
-    #     for msg in room_sender_msg_list[-10:]:  # 只取最近的10条消息
-    #         if not msg.get('is_reply'):
-    #             up_for_reply_msg_list.append(msg)
-    #             up_for_reply_msg_id_list.append(msg['id'])
-    #         else:
-    #             pass
-
-    #     # 整合最近未被回复的消息列表
-    #     recent_message_content_list = [f"\n\n{msg.get('content', '')}" for msg in up_for_reply_msg_list]
-    #     question = "\n".join(recent_message_content_list) 
-    #     print("="*50)
-    #     print(f"question: {question}")
-    #     print("="*50)
-    # else:
-    #     # 如果未开启AI，则直接使用消息内容
-    #     question = content
-    question = content
     
     # 获取AI回复
     full_answer, metadata = dify_agent.create_chat_message_stream(
-        query=question,
+        query=content,
         user_id=WX_USER_ID,
         conversation_id=conversation_id,
         inputs={"enable_ai": enable_ai}
