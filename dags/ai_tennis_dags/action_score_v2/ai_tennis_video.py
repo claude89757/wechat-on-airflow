1561cf0cb10fd992092f1139d869c48ad3e5349d
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-03-22 04:03:37 +0800

    test

--- dags/ai_tennis_dags/action_score_v2/ai_tennis_video.py
+++ dags/ai_tennis_dags/action_score_v2/ai_tennis_video.py
@@ -473,8 +473,8 @@ def process_ai_video(**context):
     extra = current_message_data.get('extra', '')  # 消息extra字段
 
     # 保存视频到微信客户端侧
-    save_dir = f"C:/Users/Administrator/Downloads/{msg_id}.mp4"
-    video_file_path = save_wx_file(wcf_ip=source_ip, id=str(msg_id), save_file_path=save_dir)
+    save_dir = f"C:/Users/Administrator/Downloads/"
+    video_file_path = save_wx_file(wcf_ip=source_ip, id=msg_id, save_file_path=save_dir)
     print(f"video_file_path: {video_file_path}")
 
     # 等待3秒
