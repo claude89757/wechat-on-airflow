835a1acb272bf2d14024868e0dca32239d3625f3
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-04-22 00:21:23 +0800

    更新 - 在网球场地监控脚本中修改短信发送功能，增加对电话号码列表的JSON反序列化支持，以提升数据处理的灵活性

--- dags/tennis_dags/sz_tennis/jdwx_watcher.py
+++ dags/tennis_dags/sz_tennis/jdwx_watcher.py
@@ -225,7 +225,7 @@ def check_tennis_courts():
 
         if up_for_send_msg_list:
             # 发送短信
-            phone_num_list = Variable.get("PHONE_NUM_LIST", default_var=[])
+            phone_num_list = Variable.get("PHONE_NUM_LIST", default_var=[], deserialize_json=True)
             for data in up_for_send_sms_list:
                 send_sms_for_news(phone_num_list, param_list=[data["date"], data["court_name"], data["start_time"], data["end_time"]])
 
