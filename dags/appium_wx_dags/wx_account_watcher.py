d5ff3513a5c67233674543f51d265d6ebe1f5f45
1osing <30589877+1osing@users.noreply.github.com>
2025-05-13 14:23:12 +0800

    fix

--- dags/appium_wx_dags/wx_account_watcher.py
+++ dags/appium_wx_dags/wx_account_watcher.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.models.variable import Variable
 from airflow.operators.python import PythonOperator
 
-from utils.appium.ssh_appium_control import get_device_id_by_adb
+from utils.appium.ssh_control import get_device_id_by_adb
 from utils.appium.wx_appium import get_wx_account_info_by_appium
 from appium_wx_dags.common.mysql_tools import init_wx_chat_records_table
 
