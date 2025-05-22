e249ae528ea52145a8c5c6780554d2f5a6c4a338
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-05-22 22:23:25 +0800

    fix

--- dags/appium_wx_dags/wx_msg_watcher.py
+++ dags/appium_wx_dags/wx_msg_watcher.py
@@ -236,7 +236,7 @@ with DAG(
     dag_id='appium_wx_msg_watcher',
     default_args={'owner': 'claude89757'},
     description='使用Appium SDK自动化微信操作',
-    schedule=timedelta(seconds=30),
+    schedule=timedelta(seconds=20),
     start_date=datetime(2025, 4, 22),
     max_active_runs=1,
     catchup=False,
