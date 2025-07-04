1e86a3a73293c6c03ed5eb28c692b38eada3e844
YuChanGongzhu <2556553872@qq.com>
2025-07-04 19:25:15 +0800

    fix

--- dags/appium_wx_dags/wx_friend_circle.py
+++ dags/appium_wx_dags/wx_friend_circle.py
@@ -4,6 +4,8 @@ from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from utils.appium.wx_appium import search_contact_name
 
+appium_server_info = Variable.get("WX_CONFIG_LIST", deserialize_json=True)
+
 with DAG(
     dag_id='friend_circle',
     default_args={'owner': 'yuchangongzhu'},
@@ -14,17 +16,15 @@ with DAG(
     tags=['个人微信', "朋友圈"],
 ) as dag:
     
-    # 直接从 Variable 获取配置
-    appium_server_info = Variable.get("WX_CONFIG_LIST", deserialize_json=True)
-    
+    # 直接从 Variable 获取配置    
     # 定义操作参数
     op_kwargs = {
-        'wx_name': appium_server_info.get('wx_name'),
-        'device_name': appium_server_info.get('device_name'),
-        'appium_url': appium_server_info.get('appium_url'),
-        'dify_api_url': appium_server_info.get('dify_api_url'),
-        'dify_api_key': appium_server_info.get('dify_api_key'),
-        'login_info': appium_server_info.get('login_info')
+        'wx_name': appium_server_info['wx_name'],
+        'device_name': appium_server_info['device_name'],
+        'appium_url': appium_server_info['appium_url'],
+        'dify_api_url': appium_server_info['dify_api_url'],
+        'dify_api_key': appium_server_info['dify_api_key'],
+        'login_info': appium_server_info['login_info']
     }
     
     # 搜索联系人
