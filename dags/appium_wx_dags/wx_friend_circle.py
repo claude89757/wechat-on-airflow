c61471ead5d0ae30119d3a808fa0b6fec5f2a5d9
YuChanGongzhu <2556553872@qq.com>
2025-07-04 19:09:09 +0800

    test

--- dags/appium_wx_dags/wx_friend_circle.py
+++ dags/appium_wx_dags/wx_friend_circle.py
@@ -2,13 +2,12 @@ from utils.appium.wx_appium import search_contact_name
 
 with DAG(
     dag_id='friend_circle',
-    default_args={'owner': 'claude89757'},
-    description='使用Appium SDK自动化微信操作',
+    default_args={'owner': 'yuchangongzhu'},
     schedule=timedelta(seconds=20),
     start_date=datetime(2025, 4, 22),
     max_active_runs=1,
     catchup=False,
-    tags=['个人微信'],
+    tags=['个人微信',"/????????"],
 ) as dag:
 
     try:
