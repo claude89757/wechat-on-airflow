13017b5a81329661e456b7df138c625e8496913e
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-07-05 10:51:54 +0800

    调整重试延迟时间，从5分钟修改为3分钟

--- dags/tennis_dags/proxy_tools/https_proxy_watcher.py
+++ dags/tennis_dags/proxy_tools/https_proxy_watcher.py
@@ -232,7 +232,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=3),
     'execution_timeout': timedelta(minutes=30),
 }
 
