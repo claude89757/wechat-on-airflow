5c1bec3cae4f6bcd66ca0c2b8adbe7cddd8eedb7
YuChanGongzhu <2556553872@qq.com>
2025-07-07 14:37:21 +0800

    test - frd circle

--- dags/appium_wx_dags/wx_friend_circle.py
+++ dags/appium_wx_dags/wx_friend_circle.py
@@ -3,11 +3,18 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from utils.appium.wx_appium import search_contact_name
+from utils.appium.wx_appium import WeChatOperator
+import os
 
 appium_server_info = Variable.get("WX_CONFIG_LIST", deserialize_json=True)
 
+def test_search_contact_name(**context):
+    search_contact_name(appium_server_url="42.193.193.179:6025", device_name='ZY22FX4H65', contact_name="1ucyEinstein", login_info={})
+
+    
+
 with DAG(
-    dag_id='friend_circle',
+    dag_id='wx_friend_circle',
     default_args={'owner': 'yuchangongzhu'},
     schedule=timedelta(seconds=20),
     start_date=datetime(2025, 4, 22),
@@ -16,8 +23,6 @@ with DAG(
     tags=['个人微信', "朋友圈"],
 ) as dag:
     
-    # 直接从 Variable 获取配置    
-    # 定义操作参数
     op_kwargs = {
         'wx_name': appium_server_info['wx_name'],
         'device_name': appium_server_info['device_name'],
@@ -29,8 +34,8 @@ with DAG(
     
     # 搜索联系人
     wx_text_handler = PythonOperator(
-        task_id='search_contact_name', 
-        python_callable=search_contact_name, 
+        task_id='test_search_contact_name', 
+        python_callable=test_search_contact_name, 
         op_kwargs=op_kwargs,
         trigger_rule='none_failed_min_one_success',
         dag=dag
