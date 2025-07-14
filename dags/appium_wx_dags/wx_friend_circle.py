f5513a793d0bdc7475871b59ddd72423c6a1ed10
Lkkkkky <1158356001@qq.com>
2025-07-14 18:46:18 +0800

    test-暂时中断AI回复dag

--- dags/appium_wx_dags/wx_friend_circle.py
+++ dags/appium_wx_dags/wx_friend_circle.py
@@ -1,19 +1,32 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
+from airflow.models import Variable,DagModel
 from utils.appium.wx_appium import search_contact_name
 from utils.appium.wx_appium import WeChatOperator
 import os
 
+
 appium_server_info = Variable.get("WX_CONFIG_LIST", deserialize_json=True)
-
 def wx_friend_circle_analyze(**context):
-    
+    wx_name = context['dag_run'].conf.get('wx_name')
     contact_name = context['dag_run'].conf.get('contact_name')
-    search_contact_name(appium_server_url="http://42.193.193.179:6050", device_name='ZY22GVV5Z2', contact_name=contact_name, login_info={})
+    for i in appium_server_info:
+        if i['wx_name']==wx_name:
+            appium_server_url=i['appium_url']
+            device_name=i['device_name']
+            dag_id=i['dag_id']
+    dag_model = DagModel.get_dagmodel(dag_id)
+    # 暂停 AI回复DAG
+    dag_model.set_is_paused(is_paused=True)
+
+    search_contact_name(appium_server_url=appium_server_url, device_name=device_name, contact_name=contact_name, login_info={})
+    # 取消暂停 DAG
+    dag_model.set_is_paused(is_paused=False)
+    
 
     
+
 
 with DAG(
     dag_id='wx_friend_circle',
