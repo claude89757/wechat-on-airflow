c8f60a834ef81cd4fdc0a805c2f4dfe555bc518e
claude89757 <138977524+claude89757@users.noreply.github.com>
2025-11-26 23:22:43 +0800

    fix

--- dags/tennis_dags/sz_tennis/jdwx_watcher.py
+++ dags/tennis_dags/sz_tennis/jdwx_watcher.py
@@ -193,7 +193,7 @@ def check_tennis_courts():
 
     # 查询空闲的球场信息
     up_for_send_data_list = []
-    for index in range(0, 2):
+    for index in range(0, 4):
         input_date = (datetime.datetime.now() + datetime.timedelta(days=index)).strftime('%Y-%m-%d')
         inform_date = (datetime.datetime.now() + datetime.timedelta(days=index)).strftime('%m-%d')
         print(f"checking {input_date}...")
